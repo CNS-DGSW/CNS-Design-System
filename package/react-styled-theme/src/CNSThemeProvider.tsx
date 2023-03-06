750335b5a8d232e21e09e7e90015b6bbe1d2d862
강성훈 <yolly050101@naver.com>
2023-03-07 08:51:00 +0900

    💥:: styled theme, design token

--- package/react-styled-theme/src/CNSThemeProvider.tsx
+++ package/react-styled-theme/src/CNSThemeProvider.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { PropsWithChildren, useMemo } from 'react';
+import { ThemeProvider as StyledThemeProvider } from 'styled-components';
+import * as colors from '@dgswcns/design-token';
 
-const CNSThemeProvider = () => {
-    return <div></div>;
+type ThemeType = { LIGHT: 'lightThemeToken'; DARK: 'darkThemeToken' };
+type ThemeKinds = keyof ThemeType;
+
+type CNSThemeProps = PropsWithChildren<{ theme: ThemeKinds }>;
+
+const CNSThemeProvider = ({ theme, children }: CNSThemeProps) => {
+    const token: ThemeType = useMemo(
+        () => ({
+            LIGHT: 'lightThemeToken',
+            DARK: 'darkThemeToken',
+        }),
+        [],
+    );
+    const themes = useMemo(() => colors[token[theme]], []);
+    return <StyledThemeProvider theme={themes}>{children}</StyledThemeProvider>;
 };
 
 export default CNSThemeProvider;
