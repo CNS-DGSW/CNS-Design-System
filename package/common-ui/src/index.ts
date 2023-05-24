5194daf85540f7a63eae28fc147b912a52e98327
진영 <bae20051018@gmail.com>
2023-05-24 10:05:17 +0900

    :sparkles:: Input ui export

--- package/common-ui/src/index.ts
+++ package/common-ui/src/index.ts
@@ -1,3 +1,4 @@
 export * from './stories/components/card/Card';
 export * from './stories/components/tooltip/Tooltip';
 export * from './stories/components/button/Button';
+export * from './stories/components/input/Input';
